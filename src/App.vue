d4a234b938a7dd650c3398cd6e549d84e57522e2
clement-gouin <gouin.cle@gmail.com>
2026-01-28 22:51:35 +0100

    feat: better button style

--- src/App.vue
+++ src/App.vue
@@ -71,21 +71,4 @@ watch(debugData, dataChanged);
 .header {
     margin: 2em 0 1rem;
 }
-
-.button {
-    display: block;
-    width: 100%;
-    text-decoration: none;
-    padding: 1em;
-    margin-bottom: 0.75em;
-    border: 1px solid var(--color-primary);
-    border-radius: 0.5em;
-    background-color: var(--background);
-    cursor: pointer;
-    font-size: 1.333em;
-}
-
-.button:hover {
-    background-color: var(--background-secondary);
-}
 </style>
